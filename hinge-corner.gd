10d9b42474023c63d08ce289294e5125cbc18664
mathiasBaumgartinger <mathias.baumgartinger@proton.me>
2025-07-31 17:00:01 +0200

    feat/WIP: add hinge-corner + hinge-shader
    With grateful credits to @kb173

--- hinge-corner.gd
+++ hinge-corner.gd
@@ -1,1 +1,12 @@
-extends Node3D
+@tool
+extends MeshInstance3D
+class_name HingeCorner
+
+@export_range(0., 360., 1.) var angle := 90. : 
+	set(new_angle):
+		angle = new_angle
+		if new_angle > 180:
+			new_angle = new_angle / 2 + 135
+		else:
+			new_angle = new_angle / 2 - 45
+		material_override.set_shader_parameter("bend", deg_to_rad(new_angle))
