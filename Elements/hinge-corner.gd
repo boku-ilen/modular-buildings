26367e800c998c74fad37968b3815de89b477ebf
mathiasBaumgartinger <mathias.baumgartinger@proton.me>
2025-10-13 17:25:03 +0200

    feat: remove debugging features from hinger-corner

--- Elements/hinge-corner.gd
+++ Elements/hinge-corner.gd
@@ -2,6 +2,7 @@
 extends MeshInstance3D
 class_name HingeCorner
 
+
 @export_range(0., 360., 1.) var angle := 90. : 
 	set(new_angle):
 		angle = new_angle
@@ -14,4 +15,6 @@ class_name HingeCorner
 		for i in range(mesh.get_surface_count()):
 			set_instance_shader_parameter("bend", deg_to_rad(new_angle))
 		
-		rotation_degrees.y = (new_angle - 90)
+		# This makes the rotation more intuitive (only one side will move the other stays static
+		# it cannot be applied to primitive meshes though, thus just for debugging
+		#rotation_degrees.y = (new_angle - 90)
